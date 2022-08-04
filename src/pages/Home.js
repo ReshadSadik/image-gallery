c2e8babc6cb0c41894d4ef59efc36b92e7c71088
ReshadSadik <reshadsadik@gmail.com>
2022-08-04 18:31:16 +0600

    added images component to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import Images from "../components/Images";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Box sx={{ padding: 5 }}>
+      <Typography sx={{ margin: "20px auto" }} variant="h4" fontWeight={"bold"}>
+        IMAGE GALLERY
+      </Typography>
+      <Images />
+    </Box>
+  );
 };
 
 export default Home;
