f14efa04e15c9d0c57c54fedc919bfcfc11bb5c1
ReshadSadik <reshadsadik@gmail.com>
2022-08-04 21:34:49 +0600

    used the image data to show to the UI

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,31 +2,53 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import IconButton from "@mui/material/IconButton";
-const Image = ({ itemData }) => {
+import { Box, CircularProgress } from "@mui/material";
+const Image = ({ images }) => {
   return (
-    <>
-      <ImageList sx={{ width: 500, height: 600, margin: "0 auto" }}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <img
-              src={`${item.img}?w=348&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=348&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading="lazy"
-            />
-            <ImageListItemBar
-              title={item.title}
-              actionIcon={
-                <IconButton
-                  sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                  aria-label={`info about ${item.title}`}
-                ></IconButton>
-              }
-            />
-          </ImageListItem>
-        ))}
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ImageList
+        sx={{
+          width: 1300,
+          height: 600,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {images ? (
+          images.map((item) => (
+            <ImageListItem key={item.id}>
+              <img
+                src={`${item?.src?.original}?w=248&fit=crop&auto=format`}
+                srcSet={`${item?.src?.original}?w=248&h&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.alt}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                title={item.alt}
+                actionIcon={
+                  <IconButton
+                    sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                    aria-label={`info about ${item.title}`}
+                  ></IconButton>
+                }
+              />
+            </ImageListItem>
+          ))
+        ) : (
+          <Box>
+            <CircularProgress />
+          </Box>
+        )}
       </ImageList>
-    </>
+    </Box>
   );
 };
 
