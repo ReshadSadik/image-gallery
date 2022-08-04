e61232282a86049b74cccbeb4af60b3b60ccade7
ReshadSadik <reshadsadik@gmail.com>
2022-08-05 01:39:44 +0600

    created function to get images using keywords

--- src/services/imageServices.js
+++ src/services/imageServices.js
@@ -1,8 +1,21 @@
 import { GETAPI } from "./API";
+import { urls } from "./urls";
 
 function getImages(page, onSuccess, onError) {
   GETAPI(
-    `https://api.pexels.com/v1/curated?page=${page}&per_page=5`,
+    `${urls.BASE_URL}/curated?page=${page}&per_page=5`,
+
+    (data) => {
+      onSuccess(data.photos);
+    },
+    (err) => {
+      onError(err);
+    }
+  );
+}
+function getImagesByName(keyword, page, onSuccess, onError) {
+  GETAPI(
+    `${urls.BASE_URL}/search?query=${keyword}&per_page=5&page=${page}`,
 
     (data) => {
       onSuccess(data.photos);
@@ -14,4 +27,5 @@ function getImages(page, onSuccess, onError) {
 }
 export const IMAGE_SERVICE = {
   getImages,
+  getImagesByName,
 };
