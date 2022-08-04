c666be70aa98ee13250e2e53c891aa47a0f0de41
ReshadSadik <reshadsadik@gmail.com>
2022-08-04 21:21:13 +0600

    created pagination to get current  page value

---
+++ src/components/PaginationContainer.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
+export default function PaginationContainer({ page, setPage }) {
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Pagination count={10} page={page} onChange={handleChange} />
+    </Stack>
+  );
+}
