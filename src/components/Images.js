701dbd412727b03f5d409b9590de077230293fc2
ReshadSadik <reshadsadik@gmail.com>
2022-08-04 18:28:14 +0600

    created images component to show images from the list

---
+++ src/components/Images.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Image from "./Image";
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+];
+
+const Images = () => {
+  return (
+    <Box sx={{ marginTop: "40px" }}>
+      <Image itemData={itemData} />
+    </Box>
+  );
+};
+
+export default Images;
